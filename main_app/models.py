d53681a1965efac35e66a82a799c43d8cf400218
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-04-29 17:53:36 -0400

    str

--- main_app/models.py
+++ main_app/models.py
@@ -23,9 +23,14 @@ class Movie(models.Model):
 		choices=GENRES,
 		default=GENRES[0][0]
 	)
+    def __str__(self):
+        return f"The Movie {self.name} has id of {self.id}"
 
 class Review(models.Model):
     comment = models.CharField(max_length=100)
     recommend = models.BooleanField(default=False)
     movies = models.ManyToManyField(Movie)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"The user {self.user} has id of {self.id}"
