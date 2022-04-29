0e7b9acd3a99e7a13194b01544efe208766c9a90
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-04-29 13:39:24 -0400

    adds movie and review models

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+GENRES = (
+    ('RC', 'Romantic Comedy'),
+    ('TR', 'Thrillers'),
+    ('DR', 'Drama'),
+    ('CO', 'Comedy'),
+    ('DO', 'Documentary'),
+    ('FA', 'Family')
+)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    genre = models.CharField(
+		max_length=2,
+		#choices
+		choices=GENRES,
+		default=GENRES[0][0]
+	)
+
+class Review(models.Model):
+    comment = models.CharField(max_length=100)
+    recommend = models.BooleanField(default=False)
+    movies = models.ManyToManyField(Movie)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
