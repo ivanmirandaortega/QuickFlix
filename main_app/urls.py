9e26c680f2816dc5a2803a3e52b78e24526b3068
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-04-29 14:58:31 -0400

    new models and forms

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ path('', views.home, name='home'),
 path('about/', views.about, name='about'),
 path('movies/', views.movies_index, name='index'),
 path('accounts/signup/', views.signup, name='signup'),
+ path('movies/create/', views.MovieCreate.as_view(), name='movies_create'),
+path('movies/<int:movie_id>/add_review/', views.add_review, name='add_review'),
+path('movies/<int:movie_id>/', views.movies_detail, name='detail'),
 ]
