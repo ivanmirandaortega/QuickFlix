72524297993274abf12905714d6488dabd3db6af
daisha.mays <daishamays@gmail.com>
2022-04-30 16:54:52 -0400

    Added search bar that can search movies by name.

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ path('accounts/signup/', views.signup, name='signup'),
 path('movies/create/', views.MovieCreate.as_view(), name='movies_create'),
 path('movies/<int:movie_id>/add_review/', views.add_review, name='add_review'),
 path('movies/<int:movie_id>/', views.movies_detail, name='detail'),
+path('movies/search_movies/', views.search_movies, name='search-movies'),
 ]
