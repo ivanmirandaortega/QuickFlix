a7162b42d11eb6df8d80c719492b0816e2a8052b
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-04-29 20:17:45 -0400

    review form

--- flickpic/settings.py
+++ flickpic/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'flickpic.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'flickpic',
+        'NAME': 'quickflix',
     }
 }
 
