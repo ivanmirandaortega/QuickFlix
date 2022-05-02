580590188b940f467fc0a333624271490794c581
Hopes-Chantel <Hopes.Chantel@gmail.com>
2022-05-01 20:41:25 -0400

    added favorites model

--- main_app/models.py
+++ main_app/models.py
@@ -10,7 +10,21 @@ GENRES = (
     ('DO', 'Documentary'),
     ('FA', 'Family')
 )
-
+class Favorite(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    genre = models.CharField(
+        max_length=2,
+        #choices
+        choices=GENRES,
+        default=GENRES[0][0]
+    )
+    #foreign key linking to a user instance
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('favorites', kwargs={'pk': self.id})
 
 class Movie(models.Model):
     name = models.CharField(max_length=100)
