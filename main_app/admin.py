7c2e75b7013afd10c1a00b4a3b52b009836fdf2c
marianaG11 <mariana.garcia.16@icloud.com>
2022-05-02 12:43:55 -0500

    added a favorites template

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Movie, Review
+from .models import Movie, Review, Favorite
 
 
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(Review)
+admin.site.register(Favorite)
--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,5 @@ path('movies/create/', views.MovieCreate.as_view(), name='movies_create'),
 path('movies/<int:movie_id>/add_review/', views.add_review, name='add_review'),
 path('movies/<int:movie_id>/', views.movies_detail, name='detail'),
 path('movies/search_movies/', views.search_movies, name='search-movies'),
+path('favorites/', views.favorites, name='favorites'), 
 ]
